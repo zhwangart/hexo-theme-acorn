de4c8fa7db67dad4d721448ee2bd4348a768486f
zhwangart <zhwangart@aliyun.com>
2022-03-05 00:26:16 +0800

    update

--- source/js/script.js
+++ source/js/script.js
@@ -11,3 +11,22 @@ headerShadow.classList.replace("header-fixed","header-absolute"); //替换
 
 feather.replace()
 
+tocbot.init({
+  // Where to render the table of contents.
+  tocSelector: '.js-toc',
+  // Where to grab the headings to build the table of contents.
+  contentSelector: '.js-toc-content',
+  // Which headings to grab inside of the contentSelector element.
+  headingSelector: 'h3, h4, h5',
+  // For headings inside relative or absolute positioned containers within content.
+  hasInnerContainers: true,
+});
+
+var tesObj = document.getElementsByName("carousel");
+	//设置 value 值为 0 选中
+	for(var i=0; i < tesObj.length; i++){
+		if (tesObj[i].value=="0"){
+			tesObj[i].checked = true;
+			break;
+			}
+	}
