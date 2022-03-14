cdb84555c573f092f0c00ea787a39b82b9d32c8e
zhwangart <zhwangart@aliyun.com>
2022-03-14 22:44:26 +0800

    Modified some CSS

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,12 @@
 document.onscroll = function() {
-var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-var headerShadow = document.getElementById("header");
-if (scrollTop > 10)
-// headerShadow.classList.add("header-fixed"); //增加
-// headerShadow.classList.remove("header-fixed"); //去除
-headerShadow.classList.replace("header-absolute","header-fixed"); //替换
-else
-headerShadow.classList.replace("header-fixed","header-absolute"); //替换
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  var headerShadow = document.getElementById("header");
+  if (scrollTop > 10)
+  // headerShadow.classList.add("header-fixed"); //增加
+  // headerShadow.classList.remove("header-fixed"); //去除
+  headerShadow.classList.replace("header-absolute","header-fixed"); //替换
+  else
+  headerShadow.classList.replace("header-fixed","header-absolute"); //替换
 }
 
 feather.replace()
