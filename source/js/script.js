c670371e1aae66f5238417b0ac77940af404c0fc
zhwangart <zhwangart@aliyun.com>
2022-04-03 21:57:57 +0800

    Fixed known bugs

--- source/js/script.js
+++ source/js/script.js
@@ -11,17 +11,6 @@ document.onscroll = function() {
 
 feather.replace()
 
-tocbot.init({
-  // Where to render the table of contents.
-  tocSelector: '.js-toc',
-  // Where to grab the headings to build the table of contents.
-  contentSelector: '.js-toc-content',
-  // Which headings to grab inside of the contentSelector element.
-  headingSelector: 'h3, h4, h5',
-  // For headings inside relative or absolute positioned containers within content.
-  hasInnerContainers: true,
-});
-
 var tesObj = document.getElementsByName("carousel");
 	//设置 value 值为 0 选中
 	for(var i=0; i < tesObj.length; i++){
